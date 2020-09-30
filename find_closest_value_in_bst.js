64c24476ac022310b2174da28d6d3d0e2549b27b
Xiaoming Fu <xmingfu86@gmail.com>
2020-09-29 23:38:03 -0700

    solve with recursion

---
+++ find_closest_value_in_bst.js
@@ -0,0 +1,21 @@
+function findClosestValueInBst(tree, target) {
+  if (!tree) {
+    return Infinity;
+  }
+  let res = tree.value;
+
+  if (target < res) {
+    let leftRes = findClosestValueInBst(tree.left, target);
+    if (Math.abs(leftRes - target) < Math.abs(res - target)) {
+      res = leftRes;
+    }
+  }
+
+  if (target > res) {
+    let rightRes = findClosestValueInBst(tree.right, target);
+    if (Math.abs(rightRes - target) > Math.abs(res - target)) {
+      res = rightRes;
+    }
+  }
+  return res;
+}

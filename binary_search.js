99897d414e1d05e4967c4a887842f7e1cab0a500
Xiaoming Fu <xmingfu86@gmail.com>
2020-10-04 23:14:00 -0700

    solve binary search

---
+++ binary_search.js
@@ -0,0 +1,17 @@
+function binarySearch(array, target) {
+  // Write your code here.
+  let i = 0;
+  let j = array.length - 1;
+
+  while (i <= j) {
+    const mid = Math.floor((j + i) / 2);
+    if (array[mid] < target) {
+      i = mid + 1;
+    } else if (array[mid] > target) {
+      j = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}

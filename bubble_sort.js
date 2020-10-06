eb03f1a1493707353c75bc957de61b0ac671a89b
Xiaoming Fu <xmingfu86@gmail.com>
2020-10-05 21:25:31 -0700

    solve with two loop

---
+++ bubble_sort.js
@@ -0,0 +1,14 @@
+// O(n^2) time | O(1) space
+function bubbleSort(array) {
+  // Write your code here.
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}

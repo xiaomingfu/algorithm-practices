d1b5ba455ce1a956a3bf79b2c8162a7303a98128
Xiaoming Fu <xmingfu86@gmail.com>
2020-10-10 22:55:40 -0700

    solve with two pointer

---
+++ smallest_difference.js
@@ -0,0 +1,24 @@
+// O(nlog(n) + nlog(m)) time | O(1) space
+function smallestDifference(arrayOne, arrayTwo) {
+  // Write your code here.
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+
+  let res = [arrayOne[0], arrayTwo[0]];
+
+  let i = 0;
+  let j = 0;
+  while (i < arrayOne.length && j < arrayTwo.length) {
+    if (Math.abs(arrayOne[i] - arrayTwo[j]) < Math.abs(res[0] - res[1])) {
+      res = [arrayOne[i], arrayTwo[j]];
+    }
+    if (arrayOne[i] < arrayTwo[j]) {
+      i++;
+    } else if (arrayOne[i] > arrayTwo[j]) {
+      j++;
+    } else {
+      return [arrayOne[i], arrayTwo[j]];
+    }
+  }
+  return res;
+}

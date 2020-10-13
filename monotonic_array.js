40c84ca9bf6b284231b3875aa90ab9c7f3ec2e8e
Xiaoming Fu <xmingfu86@gmail.com>
2020-10-13 00:02:20 -0700

    solve with two pointers

--- monotonic_array.js
+++ monotonic_array.js
@@ -3,19 +3,20 @@ function isMonotonic(array) {
   if (array.length < 2) {
     return true;
   }
+  let i = 1;
   while (i < array.length) {
     if (array[i] > array[i - 1]) {
       i++;
-      if (array[i] <= array[i - 1]) {
+      if (array[i] < array[i - 1]) {
         return false;
       }
     } else if (array[i] < array[i - 1]) {
       i++;
-      if (array[i] >= array[i - 1]) {
+      if (array[i] > array[i - 1]) {
         return false;
       }
     } else {
-      return false;
+      i++;
     }
   }
   return true;

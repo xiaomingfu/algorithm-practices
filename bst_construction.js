89374616ad051ea4fcda9b9c60ada9dbcf336232
Xiaoming Fu <xmingfu86@gmail.com>
2020-10-16 23:30:31 -0700

    need solve

---
+++ bst_construction.js
@@ -0,0 +1,23 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    return this;
+  }
+
+  contains(value) {
+    // Write your code here.
+  }
+
+  remove(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    return this;
+  }
+}

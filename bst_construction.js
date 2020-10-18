20e73e55fcdf151b909114d5c3106f7bc94c2534
Xiaoming Fu <xmingfu86@gmail.com>
2020-10-17 23:19:43 -0700

    solve insert and containmethodsgp

--- bst_construction.js
+++ bst_construction.js
@@ -7,12 +7,39 @@ class BST {
 
   insert(value) {
     // Write your code here.
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+
     // Do not edit the return statement of this method.
     return this;
   }
 
   contains(value) {
     // Write your code here.
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      }
+      return this.left.contains(value);
+    } else if (value === this.value) {
+      return true;
+    } else {
+      if (this.right === null) {
+        return false;
+      }
+      return this.right.contains(value);
+    }
   }
 
   remove(value) {

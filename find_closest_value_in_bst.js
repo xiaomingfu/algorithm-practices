0a60ff0da58690032c708b54ac0816eefade7817
Xiaoming Fu <xmingfu86@gmail.com>
2021-02-04 22:18:49 -0800

    update solutions

--- find_closest_value_in_bst.js
+++ find_closest_value_in_bst.js
@@ -19,3 +19,49 @@ function findClosestValueInBst(tree, target) {
   }
   return res;
 }
+
+// second method
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+  // average:O(nlog(n)) time, O(nlog(n)) space
+  // 	worst:O(n) time, O(n) space
+  let res = tree.value;
+  function dfs(node) {
+    if (node === null) {
+      return res;
+    }
+    if (Math.abs(node.value - target) < Math.abs(res - target)) {
+      res = node.value;
+    }
+    if (node.value > target) {
+      return dfs(node.left);
+    } else if (node.value < target) {
+      return dfs(node.right);
+    } else {
+      return res;
+    }
+  }
+  return dfs(tree);
+}
+
+// third method
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+  // 	worst: O(n) time, O(1) space
+  // 	average: O(log(n)) time, O(1) space
+  let res = tree.value;
+
+  while (tree) {
+    if (Math.abs(tree.value - target) < Math.abs(res - target)) {
+      res = tree.value;
+    }
+    if (tree.value < target) {
+      tree = tree.right;
+    } else if (tree.value > target) {
+      tree = tree.left;
+    } else {
+      break;
+    }
+  }
+  return res;
+}

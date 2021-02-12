50baf750274123bc22734dfeae9c887a454ca088
Xiaoming Fu <xmingfu86@gmail.com>
2021-02-11 23:28:47 -0800

    rewrite

--- palindrome_check.js
+++ palindrome_check.js
@@ -1,6 +1,5 @@
 // O(n) time | O(1) space
 function isPalindrome(string) {
-  // Write your code here.
   for (let i = 0, j = string.length - 1; i < j; i++, j--) {
     if (string[i] !== string[j]) {
       return false;

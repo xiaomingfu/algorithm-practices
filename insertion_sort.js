bbd4e5a963c36859d7b77067eaf0d10907b22786
Xiaoming Fu <xmingfu86@gmail.com>
2021-02-13 22:39:38 -0800

    rewrite

--- insertion_sort.js
+++ insertion_sort.js
@@ -2,14 +2,27 @@
 function insertionSort(array) {
   // Write your code here.
   for (let i = 1; i < array.length; i++) {
-    for (let j = i - 1; j >= 0; j--) {
-      if (array[j + 1] < array[j]) {
-        const temp = array[j + 1];
-        array[j + 1] = array[j];
-        array[j] = temp;
+    for (let j = i; j > 0; j--) {
+      if (array[j] < array[j - 1]) {
+        const temp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = temp;
       } else {
         break;
       }
+    }
+  }
+  return array;
+}
+
+// refactor
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      let temp = array[j];
+      array[j] = array[j - 1];
+      array[j - 1] = temp;
     }
   }
   return array;

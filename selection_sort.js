7c59631e23c741f24d84b773b560353fcd4a0241
Xiaoming Fu <xmingfu86@gmail.com>
2021-02-15 00:17:17 -0800

    rewrite

--- selection_sort.js
+++ selection_sort.js
@@ -1,20 +1,16 @@
 // O(n^2) time | O(1) space
 function selectionSort(array) {
   // Write your code here.
-  let k = 0;
   for (let i = 0; i < array.length - 1; i++) {
-    let min = array[i];
+    let minIdx = i;
     for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < min) {
-        min = array[j];
-        k = j;
+      if (array[j] < array[minIdx]) {
+        minIdx = j;
       }
     }
-    if (min !== array[i]) {
-      const temp = array[i];
-      array[i] = min;
-      array[k] = temp;
-    }
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
   return array;
 }

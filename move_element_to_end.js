e297bfdc98c663c3b7f237e552bab3355e27b829
Xiaoming Fu <xmingfu86@gmail.com>
2021-02-23 00:03:02 -0800

    rewrite

--- move_element_to_end.js
+++ move_element_to_end.js
@@ -14,3 +14,20 @@ function moveElementToEnd(array, toMove) {
   }
   return array;
 }
+// second solution
+function moveElementToEnd(array, toMove) {
+  let i = 0;
+  let j = array.length - 1;
+  while (i < j) {
+    while (i < j && array[j] === toMove) {
+      j--;
+    }
+    if (array[i] === toMove) {
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    i++;
+  }
+  return array;
+}

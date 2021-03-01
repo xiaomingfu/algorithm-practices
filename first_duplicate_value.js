618145441abc0b3a17c268ad3a23bd60637d4f78
Xiaoming Fu <xmingfu86@gmail.com>
2021-02-28 23:49:49 -0800

    solve

--- first_duplicate_value.js
+++ first_duplicate_value.js
@@ -1,3 +1,4 @@
+// O(n^2) time | O(1) space
 function firstDuplicateValue(array) {
   let smIdx = Infinity;
   for (let i = 0; i < array.length; i++) {
@@ -9,6 +10,29 @@ function firstDuplicateValue(array) {
   }
   if (smIdx !== Infinity) {
     return array[smIdx];
+  }
+  return -1;
+}
+
+// O(n) time | O(n) space
+function firstDuplicateValue(array) {
+  let dic = {};
+  for (const value of array) {
+    if (dic[value]) {
+      return value;
+    }
+    dic[value] = true;
+  }
+  return -1;
+}
+// third solution
+function firstDuplicateValue(array) {
+  let seen = new Set();
+  for (const value of array) {
+    if (seen.has(value)) {
+      return value;
+    }
+    seen.add(value);
   }
   return -1;
 }

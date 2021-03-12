1115d168976443ef4fa8151424361db270ac9901
Xiaoming Fu <xmingfu86@gmail.com>
2021-03-11 23:22:45 -0800

    add second solution

--- max_subset_sum_no_adjacent.js
+++ max_subset_sum_no_adjacent.js
@@ -12,3 +12,23 @@ function maxSubsetSumNoAdjacent(array) {
   }
   return res[res.length - 1];
 }
+
+// solution 2
+// O(n) time, O(1)space
+function maxSubsetSumNoAdjacent(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (array.length === 1) {
+    return array[0];
+  }
+  let first = array[0];
+  let second = Math.max(array[0], array[1]);
+
+  for (let i = 2; i < array.length; i++) {
+    let current = Math.max(second, first + array[i]);
+    first = second;
+    second = current;
+  }
+  return second;
+}

3bc317fa6890fce69211acead98763468512676e
Xiaoming Fu <xmingfu86@gmail.com>
2021-03-17 00:52:00 -0700

    solve

--- single_cycle_check.js
+++ single_cycle_check.js
@@ -16,3 +16,19 @@ function hasSingleCycle(array) {
   }
   return i === 0;
 }
+
+// O(n) time, O(1) space
+function hasSingleCycle(array) {
+  const n = array.length;
+  let numVisited = 0;
+  let cur = 0;
+  while (numVisited < n) {
+    if (numVisited > 0 && cur === 0) {
+      return false;
+    }
+    numVisited++;
+    cur += array[cur];
+    cur = ((cur % n) + n) % n;
+  }
+  return cur === 0;
+}

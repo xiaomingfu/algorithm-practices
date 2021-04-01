9ba42a4019b1f80e6cd5d6957efe7ac95fa0bf64
Xiaoming Fu <xmingfu86@gmail.com>
2021-04-01 00:34:15 -0700

    refactor

--- reconstruct_bst.js
+++ reconstruct_bst.js
@@ -9,19 +9,19 @@ function reconstructBst(preOrderTraversalValues) {
 		return;
 	}
 	let root = preOrderTraversalValues[0];
-	let left_sub = [];
-	let right_sub = [];
+	let leftSub = [];
+	let rightSub = [];
 	for (let i = 1; i < preOrderTraversalValues.length; i++) {
 		if (preOrderTraversalValues[i] < root) {
-			left_sub.push(preOrderTraversalValues[i]);
+			leftSub.push(preOrderTraversalValues[i]);
 		} else {
-			right_sub.push(preOrderTraversalValues[i]);
+			rightSub.push(preOrderTraversalValues[i]);
 		}
 	}
-	let left_subTree = reconstructBst(left_sub);
-	let right_subTree = reconstructBst(right_sub);
+	let leftSubTree = reconstructBst(leftSub);
+	let rightSubTree = reconstructBst(rightSub);
 	
-	return new BST(root, left_subTree, right_subTree);
+	return new BST(root, leftSubTree, rightSubTree);
 }
 
 // second solution
@@ -29,17 +29,17 @@ function reconstructBst(preOrderTraversalValues) {
 function reconstructBst(preOrderTraversalValues) {
     let i = 0;
     function helper(lb, hb) {
-        let cur = preOrderTraversalValues[i];
+        const value = preOrderTraversalValues[i];
         if (i === preOrderTraversalValues.length) {
             return;
         }
-        if (cur < lb || cur >= hb) {
+        if (value < lb || value >= hb) {
             return;
         }
         i++;
-        let left_v = helper(lb, cur);
-        let right_v = helper(cur, hb);
-        return new BST(cur, left_v, right_v);
+        const left = helper(lb, value);
+        const right = helper(value, hb);
+        return new BST(value, left, right);
     }
     return helper(-Infinity, Infinity);
 }

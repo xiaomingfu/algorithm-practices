e717bf4b4abc2b95bd0456f47bc6ae2dfff7c9de
Xiaoming Fu <xmingfu86@gmail.com>
2021-04-11 22:59:27 -0700

    solve

--- staircase_traversal.js
+++ staircase_traversal.js
@@ -16,3 +16,20 @@ function staircaseTraversal(height, maxSteps) {
   dfs(0, height);
   return cnt;
 }
+
+// second solution O(n * k) time | O(n) space
+function staircaseTraversal(height, maxSteps) {
+  let memoize = { 0: 1, 1: 1 };
+  function helper(height) {
+    if (height in memoize) {
+      return memoize[height];
+    }
+    let res = 0;
+    for (let step = 1; step <= Math.min(height, maxSteps); step++) {
+      res += helper(height - step);
+    }
+    memoize[height] = res;
+    return res;
+  }
+  return helper(height);
+}

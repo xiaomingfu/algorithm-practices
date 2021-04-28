7360eebb189aa6f595689a4367a277b312af73eb
Xiaoming Fu <xmingfu86@gmail.com>
2021-04-28 00:03:08 -0700

    second solution

--- largest_range.js
+++ largest_range.js
@@ -1,6 +1,6 @@
 function largestRange(array) {
   array.sort((a, b) => a - b);
-  let res = [-1, 0];
+  let res = [0, -1];
   let i = 0,
     j = 0;
   while (j < array.length) {
@@ -16,6 +16,37 @@ function largestRange(array) {
     }
     i = j + 1;
     j = i;
+  }
+  return res;
+}
+
+// second solution
+
+function largestRange(array) {
+  const dic = {};
+  let res = [0, -1];
+  for (let n of array) {
+    dic[n] = true;
+  }
+  for (let n of array) {
+    if (dic[n]) {
+      dic[n] = false;
+      let left = n - 1;
+      let right = n + 1;
+      while (dic[left]) {
+        dic[left] = false;
+        left--;
+      }
+      while (dic[right]) {
+        dic[right] = false;
+        right++;
+      }
+      left = left - 1;
+      right = right + 1;
+      if (right - left > res[1] - res[0]) {
+        res = [left, right];
+      }
+    }
   }
   return res;
 }

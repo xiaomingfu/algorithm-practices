b48cddacc47520e23c1cc97d1025781b67b77182
Xiaoming Fu <xmingfu86@gmail.com>
2021-06-27 00:41:15 -0700

    solved

---
+++ sort_stack.js
@@ -0,0 +1,19 @@
+function sortStack(stack) {
+  if (stack.length === 0) {
+    return stack;
+  }
+  const top = stack.pop();
+  sortStack(stack);
+  insertSortStack(stack, top);
+  return stack;
+}
+
+function insertSortStack(stack, top) {
+  if (stack.length === 0 || stack[stack.length - 1] < top) {
+    stack.push(top);
+    return;
+  }
+  const needSort = stack.pop();
+  insertSortStack(stack, top);
+  stack.push(needSort);
+}

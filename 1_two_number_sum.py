e652c5eded58e55654c504d7a3153aadec29ffbc
Xiaoming Fu <xmingfu86@gmail.com>
2022-01-29 13:32:29 -0800

    solve with python

---
+++ 1_two_number_sum.py
@@ -0,0 +1,38 @@
+# solution 1
+# onlogn time | o1 space
+def twoNumberSum(array, targetSum):
+  array.sort()
+  i = 0
+  j = len(array) - 1
+  while i < j:
+    if array[i] + array[j] > targetSum :
+      j -= 1
+    elif array[i] + array[j] < targetSum:
+      i += 1
+    else:
+      return [array[i], array[j]]
+  return []
+
+# solution 2
+# On^2 time | O1 space
+def twoNumberSum(array, targetSum) :
+  # Write your code here.
+  for i in range(len(array) - 1):
+    for j in range(i + 1, len(array)) :
+      if targetSum == array[i] + array[j]:
+        return [array[i], array[j]]
+  return []
+
+
+#   solution 3
+# 	On time, On space
+def twoNumberSum(array, targetSum) :
+  dic = {}
+  for num in array :
+    remain = targetSum - num
+    if remain in dic:
+      return [remain, num]
+    else:
+      dic[num] = True
+  return []
+

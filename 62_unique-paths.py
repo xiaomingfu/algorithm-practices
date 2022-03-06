da3d9d17caf9b94a761440a69d0bca1ace0836ea
Xiaoming Fu <xmfswe@gmail.com>
2022-03-05 23:12:41 -0800

    update

--- 62_unique-paths.py
+++ 62_unique-paths.py
@@ -8,3 +8,14 @@ class Solution:
                 return 1
             return dp(x - 1, y) + dp(x, y - 1)
         return dp(m-1, n-1)
+  # second solution
+        dp = [[1] * n for i in range(m)]
+        print(dp)
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m-1][n-1]
+        

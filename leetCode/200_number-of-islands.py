8a1e33728bc9baa57d3f1e3ab95cd3c23e38bc63
Xiaoming Fu <xmfswe@gmail.com>
2022-03-13 01:18:40 -0800

    update code for number-of-islands

--- leetCode/200_number-of-islands.py
+++ leetCode/200_number-of-islands.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/number-of-islands/
 
 from collections import deque
+from typing import List
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         # iterate grid, if grid[i][j] == 1, do dfs set val to 2, count how many not adjacent land
@@ -16,15 +17,15 @@ class Solution:
 #                     dfs(a, b)
         
     
-        def bfs(i, j):
-            f = deque([(i, j)])
-            while f:
-                for i, j in f:
-                    grid[i][j] = "2"
-                i, j = f.popleft()
+        def bfs(x, y):
+            grid[x][y] = "2"
+            q = deque([(x, y)])
+            while q:
+                i, j = q.popleft()
                 for a, b in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
                     if 0 <= a < m and 0 <= b < n and grid[a][b] == "1":
-                        f.append((a, b))
+                        grid[a][b] = "2"
+                        q.append((a, b))
         
         for i in range(m):
             for j in range(n):

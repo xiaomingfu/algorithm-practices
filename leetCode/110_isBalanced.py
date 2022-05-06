eb9fc3bb7ed5b841f5f3a9b62a3e6d41138345f2
Xiaoming Fu <xmfswe@gmail.com>
2022-05-06 01:13:45 -0700

    add second solution for isBalanced

--- leetCode/110_isBalanced.py
+++ leetCode/110_isBalanced.py
@@ -8,6 +8,17 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+      def dfs(node):
+        if node is None:
+          return True, 0
+        l_res, l_h = dfs(node.left)
+        if not l_res:
+          return False, 0
+        r_res, r_h = dfs(node.right)
+        return r_res and abs(r_h - l_h), max(l_h, r_h) + 1
+      return dfs(root)[0]
+
+      # second solution
         diff = 0
         def dfs(node):
             if node is None:

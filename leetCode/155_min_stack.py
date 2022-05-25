3b74c2edb0fee7c07e41fae03ca3e78fe25ec1e0
Xiaoming Fu <xmfswe@gmail.com>
2022-05-24 22:40:24 -0700

    min stack

---
+++ leetCode/155_min_stack.py
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/min-stack/
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val,val))
+            return
+        min_v = self.stack[-1][-1]
+        self.stack.append((val, min(min_v, val)))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][-1]

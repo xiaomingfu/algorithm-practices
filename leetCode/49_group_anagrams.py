efd6e97934dbb96af1155c4671d9ef4b1909bd4d
Xiaoming Fu <xmfswe@gmail.com>
2022-05-27 23:47:30 -0700

    group anagrams

--- leetCode/49_group_anagrams.py
+++ leetCode/49_group_anagrams.py
@@ -7,3 +7,12 @@ class Solution:
             k = "".join(sorted([c for c in s]))
             d[k].append(s)
         return list(d.values())
+
+        # second solution
+        dic = defaultdict(list)
+        for w in strs:
+            cnt = [0]*26
+            for c in w:
+                cnt[ord(c) - ord("a")] += 1
+            dic[tuple(cnt)].append(w)
+        return dic.values()

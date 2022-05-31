b9bc59135538b4fbbd97bce88eab9c7b91bf0fad
Xiaoming Fu <xmfswe@gmail.com>
2022-05-30 22:39:19 -0700

    uniquePath

--- leetCode/62_unique-paths.py
+++ leetCode/62_unique-paths.py
@@ -3,16 +3,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
       #f(i,j) = f(i - 1, j) + f(i, j - 1)
+        dp = [[1] * n for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
 
-      dp = [[1] * n for i in range(m)]
-      for i in m:
-        for j in n:
-          if i == 0 or j == 0:
-            dp[i][j] = 1
-          else:
-            dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
-      return dp[m - 1][n - 1]
-
+        dp = [[1] * n, [1] * n]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i % 2][j] = dp[(i - 1) % 2][j] + dp[i % 2][j - 1]
+        return dp[(m -1) % 2][-1]
 # top down
         @functools.cache
         def dp(x, y):

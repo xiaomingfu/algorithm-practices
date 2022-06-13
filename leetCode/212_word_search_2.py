42d7564a0d4d1e6acf2a11b61c368a6255c481ae
Xiaoming Fu <xmfswe@gmail.com>
2022-06-13 01:09:18 -0700

    word search 2

--- leetCode/212_word_search_2.py
+++ leetCode/212_word_search_2.py
@@ -1,46 +1,38 @@
 # https://leetcode.com/problems/word-search-ii/
 
-class Trie_node:
+class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.is_word = False
-    
-    def add_word(self, word):
-        cur = self
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = Trie_node()
-            cur = cur.children[c]
-        cur.is_word = True
-
+        self.children = [None] * 26
+        self.widx = -1
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        
+        m, n = len(board), len(board[0])
         # structure words in Trie
-        root = Trie_node()
-        for w in words:
-            root.add_word(w)
-         # dfs search word in board
-        row, col = len(board), len(board[0])
-        
-        res, visited =set(), set()
-        def dfs(i, j, node, str):
-            if i < 0 or j < 0 or i == row or j == col or (i, j) in visited or board[i][j] not in node.children:
-                return
+        root = TrieNode()
+        for i, w in enumerate(words):
+            node = root
+            for c in w:
+                idx = ord(c) - ord("a")
+                if node.children[idx] is None:
+                    node.children[idx] = TrieNode()
+                node = node.children[idx]
+            node.widx = i
+        res = set()
+        def dfs(i, j, node, visited):
             c = board[i][j]
-            visited.add((i, j))
-            node = node.children[c]
-            str += c
-            if node.is_word:
-                res.add(str)
-            # dfs check board in (x - 1), (x + 1), (y - 1), (y + 1) direction for word
-            dfs(i + 1, j, node, str)
-            dfs(i - 1, j, node, str)
-            dfs(i, j + 1, node, str)
-            dfs(i, j - 1, node, str)
-            visited.remove((i, j))
-        for x in range(row):
-            for y in range(col):
-                dfs(x, y, root, "")
+            idx = ord(c) - ord("a")
+            next_node = node.children[idx]
+            if next_node is not None:
+                if next_node.widx != -1:
+                    res.add(words[next_node.widx])
+                for a, b in [(i - 1, j), (i + 1, j), (i, j -1), (i, j + 1)]:
+                    if 0 <= a < m and 0 <= b < n and (a, b) not in visited:
+                        visited.add((a, b))
+                        dfs(a, b, next_node, visited)
+                        visited.remove((a, b))
+
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j, root, {(i, j)})
         return list(res)
     

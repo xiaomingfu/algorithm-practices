d1c38e0c2bd57bff78572ff672c1d3946673d6ad
Xiaoming Fu <xmfswe@gmail.com>
2022-06-15 15:42:52 -0700

    update combination sum

--- leetCode/39_combination-sum.py
+++ leetCode/39_combination-sum.py
@@ -4,15 +4,17 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def dp(i, t, arr):
-            if t == 0:
+        def bt(i, arr, remain):
+            if remain == 0:
                 res.append(arr[:])
-            elif t < 0 or i >= len(candidates):
-                return
+                
             else:
-                arr.append(candidates[i])
-                dp(i, t - candidates[i], arr)
-                arr.pop()
-                dp(i + 1, t, arr)
-        dp(0, target, [])
+                if i < len(candidates) and remain >= 0:
+                    n = candidates[i]
+                    arr.append(n)
+                    bt(i, arr, remain - n)
+                    arr.pop()
+                    bt(i + 1, arr, remain)
+        
+        bt(0, [], target)
         return res

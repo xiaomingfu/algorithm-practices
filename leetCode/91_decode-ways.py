02fcf9e0a7e14dfff5e721ea2f44262963e81e7c
Xiaoming Fu <xmfswe@gmail.com>
2022-06-27 14:35:27 -0700

    update decode ways

--- leetCode/91_decode-ways.py
+++ leetCode/91_decode-ways.py
@@ -14,3 +14,18 @@ class Solution:
                 res += dfs(i + 2)
             return res            
         return dfs(0)
+
+from functools import cache
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        @cache
+        def dfs(i):
+            if i == len(s):
+                return 1
+            if s[i] == "0":
+                return 0
+            res = dfs(i + 1)
+            if i + 1 < len(s) and (0 < int(s[i:i+2]) <= 26):
+                res += dfs(i + 2)
+            return res
+        return dfs(0)

de911d6d05ece1ebff51bfd2327385992683ff69
Xiaoming Fu <xmfswe@gmail.com>
2022-06-29 00:57:50 -0700

    bottom up decode ways

--- leetCode/91_decode-ways.py
+++ leetCode/91_decode-ways.py
@@ -2,7 +2,7 @@
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # f(i) = f(i + 1) + f(i + 2)
+        # f(i) = f(i - 1) + f(i - 2)
         @cache
         def dfs(i):
             if i == len(s):
@@ -25,7 +25,22 @@ class Solution:
             if s[i] == "0":
                 return 0
             res = dfs(i + 1)
-            if i + 1 < len(s) and (0 < int(s[i:i+2]) <= 26):
+            if i + 1 < len(s) and (10 < int(s[i:i+2]) <= 26):
                 res += dfs(i + 2)
             return res
         return dfs(0)
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        if s[0] == "0":
+            dp[1] = 0
+        else:
+            dp[1] = 1
+        for i in range(2,len(dp)):
+            if s[i - 1] != '0':
+                dp[i] = dp[i - 1]
+            if 10 <= int(s[i - 2: i]) <= 26:
+                dp[i] += dp[i - 2]
+        return dp[len(s)]

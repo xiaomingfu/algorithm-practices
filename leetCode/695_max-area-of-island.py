4f3c97b3a35a64a5c8803e1ac74bff59347cd456
Xiaoming Fu <xmfswe@gmail.com>
2022-07-28 00:01:59 -0700

    update

--- leetCode/695_max-area-of-island.py
+++ leetCode/695_max-area-of-island.py
@@ -4,7 +4,7 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         res = 0
         m, n = len(grid), len(grid[0])
-        visited = set()
+        visited = [[False] * n for _ in range(m)]
         
         def bfs(i, j):
             cnt = 0
@@ -14,15 +14,15 @@ class Solution:
                 for i,j in q:
                     cnt += 1
                     for a, b in [(i - 1,j),(i + 1, j),(i, j - 1),(i, j + 1)]:
-                        if 0 <= a < m and 0 <= b < n and (a, b) not in visited and grid[a][b] == 1:
+                        if 0 <= a < m and 0 <= b < n and not visited[a][b] and grid[a][b] == 1:
                             new_q.append((a,b))
-                            visited.add((a,b))
+                            visited[a][b] = True
                 q = new_q
             return cnt
         
         for i in range(m):
             for j in range(n):
-                if (i,j) not in visited and grid[i][j] == 1:
-                    visited.add((i,j))
+                if not visited[i][j] and grid[i][j] == 1:
+                    visited[i][j] = True
                     res = max(res, bfs(i,j))
         return res

4dd22548098248487ca2870667f2bf961fb31ee6
Xiaoming Fu <xmfswe@gmail.com>
2022-10-28 13:37:40 -0700

    union-find

--- leetCode/684_redundant-connection.py
+++ leetCode/684_redundant-connection.py
@@ -5,10 +5,28 @@ class Solution:
         ids = list(range(len(edges) + 1))
         
         def find(n):
-            if n != ids[n]:
-                n = find(ids[n])
-            return ids[n]
-        
+            # if n != ids[n]:
+            #     n = find(ids[n])
+            # return ids[n]
+
+            # nodes = []
+            # while n != ids[n]:
+            #     nodes.append(n)
+            #     n = ids[n]
+            # r = n
+            # for n in nodes:
+            #     ids[n] = r
+            # return r
+
+            r = n
+            while r != ids[r]:
+                r = ids[r]
+            while n != r:
+                t = ids[n]
+                ids[n] = r
+                n = t
+            return r
+
         def union(a, b):
             a_p = find(a)
             b_p = find(b)
